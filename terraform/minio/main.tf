edd4e9effed95f69e17cdff5aae281081846db17
Piotr Maksymiuk <50323052+ishioni@users.noreply.github.com>
2022-10-17 08:44:48 +0200

    feat: nextcloud s3 (#30)

---
+++ terraform/minio/main.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  backend "remote" {
+    organization = "ishioni"
+    workspaces {
+      name = "minio"
+    }
+  }
+
+  required_providers {
+    sops = {
+      source  = "carlpett/sops"
+      version = "0.7.1"
+    }
+    minio = {
+      source  = "aminueza/minio"
+      version = "1.7.0"
+    }
+  }
+}
+
+data "sops_file" "minio_secrets" {
+  source_file = "secret.sops.yaml"
+}
+
+provider "minio" {
+  minio_server = data.sops_file.minio_secrets.data["minio_server"]
+  # minio_region = "us-east-1"
+  minio_access_key = data.sops_file.minio_secrets.data["minio_access_key"]
+  minio_secret_key = data.sops_file.minio_secrets.data["minio_secret_key"]
+  minio_ssl        = true
+}
+
---
+++ terraform/minio/nextcloud.tf
@@ -0,0 +1,39 @@
+resource "minio_iam_user" "nextcloud" {
+  name   = "nextcloud"
+  secret = data.sops_file.minio_secrets.data["minio_nextcloud_password"]
+}
+
+resource "minio_s3_bucket" "nextcloud" {
+  bucket = "nextcloud"
+  acl    = "private"
+}
+
+data "minio_iam_policy_document" "nextcloud" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:GetBucketVersioning",
+      "s3:DeleteObject"
+    ]
+
+    resources = [
+      "${minio_s3_bucket.nextcloud.arn}/*",
+      minio_s3_bucket.nextcloud.arn
+    ]
+  }
+}
+
+resource "minio_iam_policy" "nextcloud" {
+  name   = "nextcloud"
+  policy = data.minio_iam_policy_document.nextcloud.json
+}
+
+resource "minio_iam_user_policy_attachment" "nextcloud" {
+  user_name   = minio_iam_user.nextcloud.id
+  policy_name = minio_iam_policy.nextcloud.id
+}
+
