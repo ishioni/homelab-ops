c22769a7732f783194bcc53e0dc0b5848bb52d7a
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2022-10-23 10:51:38 +0200

    init proxmox provider

---
+++ terraform/proxmox/main.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  backend "remote" {
+    organization = "ishioni"
+    workspaces {
+      name = "proxmox"
+    }
+  }
+
+  required_providers {
+    sops = {
+      source  = "carlpett/sops"
+      version = "0.7.1"
+    }
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.9.11"
+    }
+  }
+}
+
+data "sops_file" "proxmox_secrets" {
+  source_file = "secrets.sops.yaml"
+}
+
+provider "proxmox" {
+  pm_api_url          = data.sops_file.proxmox_secrets.data["pm_api_url"]
+  pm_api_token_id     = data.sops_file.proxmox_secrets.data["pm_api_token_id"]
+  pm_api_token_secret = data.sops_file.proxmox_secrets.data["pm_api_token_secret"]
+  pm_tls_insecure     = true
+  pm_debug = true
+}
+
