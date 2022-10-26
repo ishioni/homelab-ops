ea327aac7f03c278041a2ec8e094b63b227dfcaa
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2022-10-26 22:06:44 +0200

    Add tf code for k3s nodes

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -28,6 +28,5 @@ provider "proxmox" {
   pm_api_token_id     = data.sops_file.proxmox_secrets.data["pm_api_token_id"]
   pm_api_token_secret = data.sops_file.proxmox_secrets.data["pm_api_token_secret"]
   pm_tls_insecure     = true
-  pm_debug = true
+  # pm_parallel         = 2
 }
-
