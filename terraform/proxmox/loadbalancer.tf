e797eb9d8d647994a494af924df6112cda0432e6
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-06-18 16:35:23 +0200

    Don't use the LB for now..

--- terraform/proxmox/loadbalancer.tf
+++ terraform/proxmox/loadbalancer.tf
@@ -14,29 +14,29 @@ resource "time_sleep" "api-lb" {
   depends_on      = [unifi_user.api-lb]
 }
 
-resource "proxmox_lxc" "api-lb" {
-  hostname        = "talos-api"
-  tags            = "talos"
-  target_node     = "proxmox-4"
-  pool            = "infrastructure"
-  ostemplate      = "ISO:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
-  cores           = 1
-  memory          = 512
-  ssh_public_keys = data.sops_file.proxmox_secrets.data["sshkey"]
-  unprivileged    = true
-  onboot          = true
-  cmode           = "console"
-  start           = true
-  network {
-    name   = "eth0"
-    bridge = "vmbr0"
-    ip     = "dhcp"
-    tag = 4
-    hwaddr = macaddress.api-lb.address
-  }
-  rootfs {
-    storage = "local-zfs"
-    size    = "8G"
-  }
-  depends_on = [ time_sleep.api-lb ]
-}
+# resource "proxmox_lxc" "api-lb" {
+#   hostname        = "talos-api"
+#   tags            = "talos"
+#   target_node     = "proxmox-4"
+#   pool            = "infrastructure"
+#   ostemplate      = "ISO:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+#   cores           = 1
+#   memory          = 512
+#   ssh_public_keys = data.sops_file.proxmox_secrets.data["sshkey"]
+#   unprivileged    = true
+#   onboot          = true
+#   cmode           = "console"
+#   start           = true
+#   network {
+#     name   = "eth0"
+#     bridge = "vmbr0"
+#     ip     = "dhcp"
+#     tag = 4
+#     hwaddr = macaddress.api-lb.address
+#   }
+#   rootfs {
+#     storage = "local-zfs"
+#     size    = "8G"
+#   }
+#   depends_on = [ time_sleep.api-lb ]
+# }
