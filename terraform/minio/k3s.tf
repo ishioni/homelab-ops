1cda946834a72c68b7ae0e3eced44a7414b35ce5
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-06-29 12:40:18 +0200

    Migrated to new minio

--- terraform/minio/k3s.tf
+++
@@ -1,7 +0,0 @@
-module "s3_k3s" {
-  source      = "./modules/minio"
-  vault       = "Homelab"
-  bucket_name = "k3s"
-  # The OP provider converts the fields with toLower!
-  user_secret_item = "s3_secret_key"
-}
