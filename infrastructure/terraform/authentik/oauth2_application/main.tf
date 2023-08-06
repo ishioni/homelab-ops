c59f8ca40ec6b1ae21205e41b51b8d8b4e37e96b
Piotr Maksymiuk <50323052+ishioni@users.noreply.github.com>
2023-08-06 17:03:15 +0200

    Feat: authentik tf 1password (#1057)

--- infrastructure/terraform/authentik/oauth2_application/main.tf
+++ infrastructure/terraform/authentik/oauth2_application/main.tf
@@ -19,37 +19,6 @@ data "authentik_scope_mapping" "scopes" {
   ]
 }
 
-# resource "authentik_provider_oauth2" "immich" {
-#   name                       = "immich-oidc"
-#   client_id                  = data.sops_file.authentik_secrets.data["immich_client_id"]
-#   client_secret              = data.sops_file.authentik_secrets.data["immich_client_secret"]
-#   authorization_flow         = resource.authentik_flow.provider-authorization-implicit-consent.uuid
-#   signing_key                = data.authentik_certificate_key_pair.generated.id
-#   client_type                = "confidential"
-#   include_claims_in_id_token = true
-#   issuer_mode                = "per_provider"
-#   sub_mode                   = "user_username"
-#   access_code_validity       = "hours=24"
-#   property_mappings = concat(
-#     data.authentik_scope_mapping.scopes.ids
-#   )
-#   redirect_uris = [
-#     "https://photos.${data.sops_file.authentik_secrets.data["cluster_domain"]}/auth/login",
-#     "app.immich:/"
-#   ]
-# }
-
-# resource "authentik_application" "immich" {
-#   name              = "immich"
-#   slug              = "immich"
-#   group             = "Media"
-#   protocol_provider = resource.authentik_provider_oauth2.immich.id
-#   meta_icon         = "https://github.com/immich-app/immich/raw/main/docs/static/img/favicon.png"
-#   meta_description  = "Photo managment"
-#   meta_launch_url   = "https://photos.${data.sops_file.authentik_secrets.data["cluster_domain"]}"
-#   open_in_new_tab   = true
-# }
-
 resource "authentik_provider_oauth2" "oauth2-application" {
   name                       = var.name
   client_id                  = var.client_id
