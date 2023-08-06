d1e5fc78bbac26e116faa70bffa3f62027970f3d
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-08-06 13:23:28 +0200

    move outpost to system

--- infrastructure/terraform/authentik/applications.tf
+++ infrastructure/terraform/authentik/applications.tf
@@ -1,36 +1,3 @@
-resource "authentik_outpost" "proxyoutpost" {
-  name               = "proxy-outpost"
-  type               = "proxy"
-  service_connection = authentik_service_connection_kubernetes.local.id
-  protocol_providers = [
-    module.proxy-transmission.id,
-    module.proxy-prowlarr.id,
-    module.proxy-radarr.id,
-    module.proxy-sonarr.id,
-    module.proxy-hajimari.id
-  ]
-  config = jsonencode({
-    authentik_host          = "https://auth.${data.sops_file.authentik_secrets.data["cluster_domain"]}",
-    authentik_host_insecure = false,
-    authentik_host_browser  = "",
-    log_level               = "debug",
-    object_naming_template  = "ak-outpost-%(name)s",
-    docker_network          = null,
-    docker_map_ports        = true,
-    docker_labels           = null,
-    container_image         = null,
-    kubernetes_replicas     = 1,
-    kubernetes_namespace    = "security",
-    kubernetes_ingress_annotations = {
-      "cert-manager.io/cluster-issuer" = "letsencrypt-production"
-    },
-    kubernetes_ingress_secret_name = "proxy-outpost-tls",
-    kubernetes_service_type        = "ClusterIP",
-    kubernetes_disabled_components = [],
-    kubernetes_image_pull_secrets  = []
-  })
-}
-
 module "proxy-transmission" {
   source             = "./proxy_application"
   name               = "Transmission"
