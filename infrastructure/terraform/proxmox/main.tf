c2da05682657970a9becda598d291dc766bf5748
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-08-06 19:20:01 +0200

    Get rid of sops in proxmox tf code

--- infrastructure/terraform/proxmox/main.tf
+++ infrastructure/terraform/proxmox/main.tf
@@ -8,10 +8,6 @@ terraform {
   }
 
   required_providers {
-    sops = {
-      source  = "carlpett/sops"
-      version = "0.7.2"
-    }
     proxmox = {
       source  = "Telmate/proxmox"
       version = "2.9.14"
@@ -27,20 +23,30 @@ terraform {
   }
 }
 
-data "sops_file" "proxmox_secrets" {
-  source_file = "secrets.sops.yaml"
+module "secret_pve" {
+  # Remember to export OP_CONNECT_HOST and OP_CONNECT_TOKEN
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "Homelab"
+  item   = "proxmox"
+}
+
+module "secret_unifi" {
+  # Remember to export OP_CONNECT_HOST and OP_CONNECT_TOKEN
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "Homelab"
+  item   = "unifi"
 }
 
 provider "proxmox" {
-  pm_api_url          = data.sops_file.proxmox_secrets.data["pm_api_url"]
-  pm_api_token_id     = data.sops_file.proxmox_secrets.data["pm_api_token_id"]
-  pm_api_token_secret = data.sops_file.proxmox_secrets.data["pm_api_token_secret"]
+  pm_api_url          = module.secret_pve.fields.pm_api_url
+  pm_api_token_id     = module.secret_pve.fields.pm_api_token_id
+  pm_api_token_secret = module.secret_pve.fields.pm_api_token_secret
   pm_tls_insecure     = true
   # pm_parallel         = 2
 }
 
 provider "unifi" {
-  username = data.sops_file.proxmox_secrets.data["unifi_username"]
-  password = data.sops_file.proxmox_secrets.data["unifi_password"]
-  api_url  = data.sops_file.proxmox_secrets.data["unifi_url"]
+  username = module.secret_unifi.fields.username
+  password = module.secret_unifi.fields.password
+  api_url  = module.secret_unifi.fields.api_url
 }
