913d0e47bd70665671fc3dac3d1176ccc2e4da48
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-08-11 19:42:53 +0200

    Add wifi

--- infrastructure/terraform/unifi/wifi.tf
+++ infrastructure/terraform/unifi/wifi.tf
@@ -1,12 +1,26 @@
 resource "unifi_wlan" "MoviNet" {
   name = "MoviNet"
   network_id = unifi_network.LAN.id
-  passphrase = "januszesieci2018"
+  passphrase = module.secret_unifi.fields.movinet_password
   security = "wpapsk"
   fast_roaming_enabled = true
   multicast_enhance = true
   bss_transition = true
   no2ghz_oui = true
+
+  ap_group_ids = [data.unifi_ap_group.default.id]
+  user_group_id = data.unifi_user_group.default.id
+}
+
+resource "unifi_wlan" "MoviIOT" {
+  name = "MoviIOT"
+  network_id = unifi_network.IOT.id
+  passphrase = module.secret_unifi.fields.moviiot_password
+  security = "wpapsk"
+  multicast_enhance = true
+  bss_transition = true
+  no2ghz_oui = false
+  wlan_band = "2g"
 
   ap_group_ids = [data.unifi_ap_group.default.id]
   user_group_id = data.unifi_user_group.default.id
