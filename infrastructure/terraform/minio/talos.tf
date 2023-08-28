1f44918e26086846de3cdaa1d182b534b1d5fa72
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-08-28 23:10:40 +0200

    Init talos backups bucket

---
+++ infrastructure/terraform/minio/talos.tf
@@ -0,0 +1,17 @@
+module "talos" {
+  source      = "./modules/minio"
+  vault       = "Homelab"
+  bucket_name = "talos"
+  # The OP provider converts the fields with toLower!
+  user_secret_item = "s3_secret_key"
+  versioning = true
+}
+
+resource "minio_ilm_policy" "talos-expiration" {
+  bucket = module.talos.bucket
+
+  rule {
+    id = "expire-14d"
+    expiration = "14d"
+  }
+}
