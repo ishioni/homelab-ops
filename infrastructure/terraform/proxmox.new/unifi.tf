059d3fdbdbd18fa28155d4eac8284f5e89e4f51d
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-09-11 10:40:18 +0200

    Try lxc, and fail

--- infrastructure/terraform/proxmox.new/unifi.tf
+++ infrastructure/terraform/proxmox.new/unifi.tf
@@ -1,3 +1,23 @@
-data "unifi_network" "kubernetes" {
-  name = "Kubernetes"
-}
+# resource "proxmox_virtual_environment_container" "unifi" {
+#     description = "Unifi controller managed by terraform"
+
+#     node_name = "proxmox-4"
+#     vm_id = 102
+
+#     tags = [ "ubuntu" ]
+
+#     cpu {
+#         cores = 1
+#     }
+
+#     memory {
+#         dedicated = 2
+#     }
+
+#     disk {
+#         datastore_id = "local-zfs"
+#         size = 8
+#     }
+# }
+
+# Import broken for lxc :()
