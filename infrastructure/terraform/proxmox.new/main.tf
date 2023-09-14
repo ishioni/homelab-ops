6ff7f6e03b91f3c182dfecba9bdcc56616bb5c21
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2023-09-14 18:54:00 +0200

    Add unifi terraform

--- infrastructure/terraform/proxmox.new/main.tf
+++ infrastructure/terraform/proxmox.new/main.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.31.0"
+      version = "0.32.0"
     }
     unifi = {
       source  = "paultyng/unifi"
