35709b0c3f2e9ceeab6d482ca75a72b250abed31
Piotr Maksymiuk <50323052+ishioni@users.noreply.github.com>
2023-09-28 11:13:38 +0200

    feat: tf-unifi (#1269)

---
+++ infrastructure/terraform/unifi/main.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  backend "remote" {
+    organization = "ishioni"
+    workspaces {
+      name = "unifi"
+    }
+  }
+
+  required_providers {
+    unifi = {
+      source  = "paultyng/unifi"
+      version = "0.41.0"
+    }
+    macaddress = {
+      source  = "ivoronin/macaddress"
+      version = "0.3.2"
+    }
+  }
+}
+
+module "secret_unifi" {
+  # Remember to export OP_CONNECT_HOST and OP_CONNECT_TOKEN
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "Homelab"
+  item   = "unifi"
+}
+
+provider "unifi" {
+  username = module.secret_unifi.fields.username
+  password = module.secret_unifi.fields.password
+  api_url  = module.secret_unifi.fields.api_url
+}
