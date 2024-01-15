afec3a8d4a19d34327920b71d1880ff783d1b420
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-01-15 16:22:19 +0100

    final proxmox config

--- infrastructure/terraform/proxmox/talos.tf
+++ infrastructure/terraform/proxmox/talos.tf
@@ -1,39 +1,39 @@
-data "unifi_network" "Kubernetes" {
-  name = "Kubernetes"
+data "unifi_network" "Servers" {
+  name = "Servers"
 }
 
-module "controlplanes" {
+module "talos-controlplanes" {
   source          = "./talos-node"
-  oncreate        = true
-  count           = 3
+  oncreate        = false
+  count           = 1
   machine_name    = "master-${count.index}"
-  vmid            = sum([4010, count.index])
+  vmid            = sum([2030, count.index])
   target_node     = "proxmox-${count.index + 1}"
-  iso_path        = var.talos_image
+  iso_path        = "ISO:iso/talos-v1.6.1-cp.iso"
   timeout_stop_vm = 300
   qemu_agent      = true
   cpu_cores       = 4
   memory          = 4 * 1024
-  vlan_id         = data.unifi_network.Kubernetes.vlan_id
+  vlan_id         = data.unifi_network.Servers.vlan_id
   storage         = "local-zfs"
   storage_size    = 20
 }
 
-module "workers" {
+module "talos-workers" {
   source          = "./talos-node"
-  oncreate        = true
+  oncreate        = false
   count           = 3
   machine_name    = "worker-${count.index}"
-  vmid            = sum([4020, count.index])
+  vmid            = sum([2040, count.index])
   target_node     = "proxmox-${count.index + 1}"
-  iso_path        = var.talos_image
+  iso_path        = "ISO:iso/talos-v1.6.1-worker.iso"
   timeout_stop_vm = 300
   qemu_agent      = true
   uefi             = false #gvtd doesn't play nice with UEFI
   cpu_cores       = 8
   memory          = 24 * 1024
   gpu_gvtd        = true
-  vlan_id         = data.unifi_network.Kubernetes.vlan_id
+  vlan_id         = data.unifi_network.Servers.vlan_id
   storage         = "local-zfs"
   storage_size    = 40
 }
