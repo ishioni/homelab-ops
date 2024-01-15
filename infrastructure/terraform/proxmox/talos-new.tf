340a297acded4741ebe1d3e30901991ff8ac80e0
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-01-15 15:44:36 +0100

    don't overwrite the new cluster for now

---
+++ infrastructure/terraform/proxmox/talos-new.tf
@@ -0,0 +1,39 @@
+data "unifi_network" "Servers" {
+  name = "Servers"
+}
+
+module "talos-controlplanes" {
+  source          = "./talos-node"
+  oncreate        = false
+  count           = 1
+  machine_name    = "master-${count.index}"
+  vmid            = sum([2030, count.index])
+  target_node     = "proxmox-${count.index + 1}"
+  iso_path        = var.talos_image
+  timeout_stop_vm = 300
+  qemu_agent      = true
+  cpu_cores       = 4
+  memory          = 4 * 1024
+  vlan_id         = data.unifi_network.Kubernetes.vlan_id
+  storage         = "local-zfs"
+  storage_size    = 20
+}
+
+# module "talos-workers" {
+#   source          = "./talos-node"
+#   oncreate        = false
+#   count           = 3
+#   machine_name    = "worker-${count.index}"
+#   vmid            = sum([2040, count.index])
+#   target_node     = "proxmox-${count.index + 1}"
+#   iso_path        = var.talos_image
+#   timeout_stop_vm = 300
+#   qemu_agent      = true
+#   uefi             = false #gvtd doesn't play nice with UEFI
+#   cpu_cores       = 8
+#   memory          = 24 * 1024
+#   gpu_gvtd        = true
+#   vlan_id         = data.unifi_network.Kubernetes.vlan_id
+#   storage         = "local-zfs"
+#   storage_size    = 40
+# }
--- infrastructure/terraform/proxmox/talos.tf
+++ infrastructure/terraform/proxmox/talos.tf
@@ -1,30 +1,30 @@
-data "unifi_network" "Servers" {
-  name = "Servers"
+data "unifi_network" "Kubernetes" {
+  name = "Kubernetes"
 }
 
 module "controlplanes" {
   source          = "./talos-node"
-  oncreate        = false
+  oncreate        = true
   count           = 3
   machine_name    = "master-${count.index}"
-  vmid            = sum([2030, count.index])
+  vmid            = sum([4010, count.index])
   target_node     = "proxmox-${count.index + 1}"
   iso_path        = var.talos_image
   timeout_stop_vm = 300
   qemu_agent      = true
   cpu_cores       = 4
   memory          = 4 * 1024
-  vlan_id         = data.unifi_network.Servers.vlan_id
+  vlan_id         = data.unifi_network.Kubernetes.vlan_id
   storage         = "local-zfs"
   storage_size    = 20
 }
 
 module "workers" {
   source          = "./talos-node"
-  oncreate        = false
+  oncreate        = true
   count           = 3
   machine_name    = "worker-${count.index}"
-  vmid            = sum([2040, count.index])
+  vmid            = sum([4020, count.index])
   target_node     = "proxmox-${count.index + 1}"
   iso_path        = var.talos_image
   timeout_stop_vm = 300
@@ -33,7 +33,7 @@ module "workers" {
   cpu_cores       = 8
   memory          = 24 * 1024
   gpu_gvtd        = true
-  vlan_id         = data.unifi_network.Servers.vlan_id
+  vlan_id         = data.unifi_network.Kubernetes.vlan_id
   storage         = "local-zfs"
   storage_size    = 40
 }
