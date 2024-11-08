38fa2761224fef9adf5bfe00511db292452ad29b
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2024-11-08 18:55:45 +0100

    fix: tofu provider changed

--- infrastructure/terraform/proxmox/main.tf
+++ infrastructure/terraform/proxmox/main.tf
@@ -5,7 +5,7 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    force_path_style            = true
+    use_path_style              = true
   }
 
   required_providers {
