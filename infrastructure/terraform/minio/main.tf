9bbaef8c3b8177ea73abffdf1eb4a83044dbf903
Piotr Maksymiuk <50323052+ishioni@users.noreply.github.com>
2024-12-01 18:28:45 +0100

    remove truenas k3s (#2909)

--- infrastructure/terraform/minio/main.tf
+++ infrastructure/terraform/minio/main.tf
@@ -5,7 +5,6 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    use_path_style              = true
   }
 
   required_providers {
