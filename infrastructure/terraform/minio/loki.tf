15fb92eefefb7c1cc91dab3e43fdc0e74ad982f0
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2025-01-02 14:01:51 +0100

    chore: cleanup loki stuff

--- infrastructure/terraform/minio/loki.tf
+++
@@ -1,7 +0,0 @@
-module "s3_loki" {
-  source      = "./modules/minio"
-  vault       = "Homelab"
-  bucket_name = "loki"
-  # The OP provider converts the fields with toLower!
-  user_secret_item = "S3_SECRET_KEY"
-}
