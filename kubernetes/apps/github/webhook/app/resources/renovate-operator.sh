adebd8beabbcd20eb85ec7d955c6a3287e61f437
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2026-01-05 00:21:32 +0100

    fix(webhook): make the hook work

--- kubernetes/apps/github/webhook/app/resources/renovate-operator.sh
+++ kubernetes/apps/github/webhook/app/resources/renovate-operator.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set -x
+
 # Incoming arguments
 JOB=${1:-}
 NAMESPACE=${2:-}
@@ -8,15 +8,6 @@ PROJECT=${3:-}
 
 # URL encode the project name
 PROJECT=$(echo "${PROJECT}" | jq -Rr @uri)
-echo ${JOB}
-echo ${NAMESPACE}
-echo ${PROJECT}
-echo "Number of arguments: $#"
-echo "All arguments (\$@): [$@]"
-echo "All arguments individually:"
-for i in "$@"; do
-  echo "  [$i]"
-done
 
 curl -s -X POST \
   "http://renovate-operator-webhook.github.svc.cluster.local:8082/webhook/v1/schedule?job=${JOB}&namespace=${NAMESPACE}&project=${PROJECT}"
