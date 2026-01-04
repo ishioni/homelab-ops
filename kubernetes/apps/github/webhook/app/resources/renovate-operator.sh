063e31bb8f075b7b0f837128c7337ee3b605d8c9
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2026-01-05 00:13:56 +0100

    test: more debugging

--- kubernetes/apps/github/webhook/app/resources/renovate-operator.sh
+++ kubernetes/apps/github/webhook/app/resources/renovate-operator.sh
@@ -11,6 +11,12 @@ PROJECT=$(echo "${PROJECT}" | jq -Rr @uri)
 echo ${JOB}
 echo ${NAMESPACE}
 echo ${PROJECT}
+echo "Number of arguments: $#"
+echo "All arguments (\$@): [$@]"
+echo "All arguments individually:"
+for i in "$@"; do
+  echo "  [$i]"
+done
 
 curl -s -X POST \
   "http://renovate-operator-webhook.github.svc.cluster.local:8082/webhook/v1/schedule?job=${JOB}&namespace=${NAMESPACE}&project=${PROJECT}"
