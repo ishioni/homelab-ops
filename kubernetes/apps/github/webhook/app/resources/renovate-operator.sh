e43445fff1b38eaed8182d429ff003004ee3cac7
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2026-01-04 23:52:01 +0100

    test(webhook): try to debug

--- kubernetes/apps/github/webhook/app/resources/renovate-operator.sh
+++ kubernetes/apps/github/webhook/app/resources/renovate-operator.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+set -x
 # Incoming arguments
 JOB=${1:-}
 NAMESPACE=${2:-}
@@ -8,6 +8,9 @@ PROJECT=${3:-}
 
 # URL encode the project name
 PROJECT=$(echo "${PROJECT}" | jq -Rr @uri)
+echo ${JOB}
+echo ${NAMESPACE}
+echo ${PROJECT}
 
 curl -s -X POST \
   "http://renovate-operator-webhook.github.svc.cluster.local:8082/webhook/v1/schedule?job=${JOB}&namespace=${NAMESPACE}&project=${PROJECT}"
