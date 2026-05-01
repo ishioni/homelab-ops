e23b4b1e7f43311ce9420e132e0f7149edc62510
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2026-05-01 23:50:56 +0200

    chore(authentik): cleanup

--- terraform/authentik/expressions/openid-scope-nextcloud.py
+++
@@ -1,4 +0,0 @@
-return {
-    "nextcloudAdmin": user.attributes.get("nextcloudAdmin"),
-    "nextcloudQuota": user.attributes.get("nextcloudQuota",user.group_attributes().get("defaultQuota", "100 MB"))
-}
