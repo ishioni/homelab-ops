fd7522368446e31a411b20a2b12580ab21dcf020
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2026-05-31 16:17:18 +0200

    fix(webhook): include repository html_url for doco-cd

--- kubernetes/apps/github/webhook/app/resources/github-push-doco.sh
+++ kubernetes/apps/github/webhook/app/resources/github-push-doco.sh
@@ -13,12 +13,14 @@ WEBHOOK_URL="${7:?}"
 export WEBHOOK_SECRET=${GITHUB_WEBHOOK_SECRET:?missing secret}
 
 CLONE_URL="https://github.com/${GITHUB_REPOSITORY}.git"
+WEB_URL="https://github.com/${GITHUB_REPOSITORY}"
 
 PAYLOAD=$(jq -n \
   --arg ref "${GITHUB_REF}" \
   --arg before "${GITHUB_SHA_BEFORE}" \
   --arg after "${GITHUB_SHA_AFTER}" \
   --arg clone_url "${CLONE_URL}" \
+  --arg html_url "${WEB_URL}" \
   --arg reponame "${GITHUB_REPOSITORY##*/}" \
   --arg repo_fullname "${GITHUB_REPOSITORY}" \
   --arg pusher_name "${GITHUB_ACTOR_NAME}" \
@@ -30,7 +32,8 @@ PAYLOAD=$(jq -n \
     repository: {
       name: $reponame,
       full_name: $repo_fullname,
-      clone_url: $clone_url
+      clone_url: $clone_url,
+      html_url: $html_url
     },
     pusher: {
       name: $pusher_name,
